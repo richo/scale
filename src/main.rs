fe0b32044227855d7339679b4f8d952db5458e05
Richo Healey <richo@psych0tik.net>
2023-09-27 12:53:20 -0700

    Sending weight seems to work

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@
 use esp_backtrace as _;
 use esp_println::println;
 use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay, IO};
+use bleps::{
+    ad_structure::{
+        create_advertising_data, AdStructure, BR_EDR_NOT_SUPPORTED, LE_GENERAL_DISCOVERABLE,
+    },
+    attribute_server::{AttributeServer, NotificationData, WorkResult},
+    Ble, HciConnector,
+};
+use bleps_macros::gatt;
 
 use hx711;
 use nb::block;
@@ -11,10 +19,12 @@ use nb::block;
 use embedded_hal::blocking::delay::DelayUs;
 use embedded_hal::digital::v2::{InputPin, OutputPin};
 
-use esp_wifi::{initialize, EspWifiInitFor};
+use esp_wifi::{ble::controller::BleConnector, initialize, EspWifiInitFor};
 
 use hal::{timer::TimerGroup, Rng};
 use core::sync::atomic::{AtomicI32, Ordering};
+// use core::thread;
+use uuid::Uuid;
 
 static weight: AtomicI32 = AtomicI32::new(0);
 
@@ -74,7 +84,7 @@ fn main() -> ! {
         &mut system.peripheral_clock_control,
     )
     .timer0;
-    let _init = initialize(
+    let init = initialize(
         EspWifiInitFor::Ble,
         timer,
         Rng::new(peripherals.RNG),
@@ -82,6 +92,9 @@ fn main() -> ! {
         &clocks,
     )
     .unwrap();
+
+    let (_, mut bluetooth, ..) = peripherals.RADIO.split();
+
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
@@ -104,12 +117,126 @@ fn main() -> ! {
 
     let factor = (273577.0 + 319663.0) / 807.5;
 
-    loop {
-        let l = left.corrected_value(left_tare);
-        let r = right.corrected_value(right_tare);
-        let w = (l as f32 / left_scale) + (r as f32 / right_scale);
-        weight.store((((l + r) as f32 / factor) * 10.0 ) as i32, Ordering::Relaxed);
-        log::info!("val: {w}\t{}\t{}", l, r);
+    // loop {
+    //     let l = left.corrected_value(left_tare);
+    //     let r = right.corrected_value(right_tare);
+    //     let w = (l as f32 / left_scale) + (r as f32 / right_scale);
+    //     weight.store((((l + r) as f32 / factor) * 10.0 ) as i32, Ordering::Relaxed);
+    //     log::info!("val: {w}\t{}\t{}", l, r);
 
+    // }
+    loop {
+        let connector = BleConnector::new(&init, &mut bluetooth);
+        let hci = HciConnector::new(connector, esp_wifi::current_millis);
+        let mut ble = Ble::new(&hci);
+
+        println!("{:?}", ble.init());
+        println!("{:?}", ble.cmd_set_le_advertising_parameters());
+        println!(
+            "{:?}",
+            ble.cmd_set_le_advertising_data(
+                create_advertising_data(&[
+                    AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
+                    AdStructure::ServiceUuids16(&[Uuid::Uuid16(0x1809)]),
+                    AdStructure::CompleteLocalName("butts_scale"),
+                ]).unwrap()
+            )
+        );
+        println!("{:?}", ble.cmd_set_le_advertise_enable(true));
+
+        println!("started advertising");
+
+        let mut rf = |_offset: usize, data: &mut [u8]| {
+            data[..20].copy_from_slice(&b"Hello Bare-Metal BLE"[..]);
+            17
+        };
+        let mut wf = |offset: usize, data: &[u8]| {
+            println!("RECEIVED: {} {:?}", offset, data);
+        };
+
+        let mut wf2 = |offset: usize, data: &[u8]| {
+            println!("RECEIVED: {} {:?}", offset, data);
+        };
+
+        let mut rf3 = |_offset: usize, data: &mut [u8]| {
+            data[..5].copy_from_slice(&b"Hola!"[..]);
+            5
+        };
+        let mut wf3 = |offset: usize, data: &[u8]| {
+            println!("RECEIVED: Offset {}, data {:?}", offset, data);
+        };
+
+        gatt!([service {
+            uuid: "937312e0-2354-11eb-9f10-fbc30a62cf38",
+            characteristics: [
+                characteristic {
+                    uuid: "937312e0-2354-11eb-9f10-fbc30a62cf38",
+                    read: rf,
+                    write: wf,
+                },
+                characteristic {
+                    uuid: "957312e0-2354-11eb-9f10-fbc30a62cf38",
+                    write: wf2,
+                },
+                characteristic {
+                    name: "weight",
+                    uuid: "0000FFF4-0000-1000-8000-00805F9B34FB",
+                    notify: true,
+                    read: rf3,
+                    write: wf3,
+                },
+            ],
+        },]);
+
+        let mut srv = AttributeServer::new(&mut ble, &mut gatt_attributes);
+
+        loop {
+            let mut notification = None;
+
+            let l = left.corrected_value(left_tare);
+            let r = right.corrected_value(right_tare);
+            // TODO(richo) figure out how to do this at 10hz
+            let w = (l as f32 / left_scale) + (r as f32 / right_scale);
+
+            // int repr of grams *10
+            let i = (w * 10.0) as i16;
+
+            let mut cccd = [0u8; 1];
+            if let Some(1) = srv.get_characteristic_value(
+                weight_notify_enable_handle,
+                0,
+                &mut cccd,
+            ) {
+                let mut payload = [0x03, 0xCE, 0x00, 0x00, 0x00, 0x00, 0x00];
+                payload[2..4].copy_from_slice(&i.to_be_bytes());
+
+                // Calculate the xor thingy
+                let mut xor = 0x00;
+                for b in &payload[..] {
+                    xor ^= b;
+                }
+                payload[6] = xor;
+
+                // if notifications enabled
+                if cccd[0] == 1 {
+                    notification = Some(NotificationData::new(
+                            weight_handle,
+                            &payload[..],
+                    ));
+                }
+            }
+
+
+            match srv.do_work_with_notification(notification) {
+                Ok(res) => {
+                    if let WorkResult::GotDisconnected = res {
+                        break;
+                    }
+                }
+                Err(err) => {
+                    println!("{:?}", err);
+                }
+            }
+        }
     }
 }
